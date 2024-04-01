bc9dfdb6d63fbea417aa9d0ff2de806df7166c4d
Rafael <rafaelopires@icloud.com>
2024-03-31 23:12:18 -0300

    Initial Commit

---
+++ SSV-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSV-app
+//
+//  Created by Rafael Oliveira on 31/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
